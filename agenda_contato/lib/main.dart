e06952418c7ef335edbe516cd76996352b5aea21
Adailton Silva <adailton.silva10@live.com>
2019-08-06 16:51:26 -0300

    telas, db

--- agenda_contato/lib/main.dart
+++ agenda_contato/lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:agenda_contato/ui/contact_page.dart';
 import 'package:flutter/material.dart';
+import 'package:agenda_contato/ui/home_page.dart';
 
 void main(){
   runApp(MaterialApp(
-    
+    home: ContactPage(),
+    debugShowCheckedModeBanner: false,
   ));
 }
