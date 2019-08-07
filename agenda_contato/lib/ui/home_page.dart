796a80d1b95b2eac300d60e2254c8cd01e7db0ee
Adailton Silva <adailton.silva10@live.com>
2019-08-07 17:03:58 -0300

    tela de contatos

--- agenda_contato/lib/ui/home_page.dart
+++ agenda_contato/lib/ui/home_page.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:agenda_contato/helpers/contact_help.dart';
+import 'package:agenda_contato/ui/contact_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -16,12 +17,7 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState(){
     super.initState();
-
-    helper.getAllContacts().then((list){
-      setState(() {
-       contacts = list; 
-      });
-    });
+    _getAllContacts();   
   }
 
   @override
@@ -34,7 +30,9 @@ class _HomePageState extends State<HomePage> {
       ),
       backgroundColor: Colors.white,
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: (){
+          _showContactPage();
+        },
         child: Icon(Icons.add),
         backgroundColor: Colors.red,
       ),
@@ -88,6 +86,86 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
+      onTap: (){
+        _showOptions(context, index);
+      },
     );
+  }
+
+  void _showContactPage({Contact contact}) async{
+    final recContact = await Navigator.push(context, 
+      MaterialPageRoute(builder: (context) => ContactPage(contact: contact,))
+    );
+    if(recContact != null){
+      if(contact != null){
+        await helper.updateContact(recContact);
+      }else{
+        await helper.saveContact(recContact);
+      }
+      _getAllContacts();
+    }
+  }
+
+  void _showOptions(BuildContext context, int index){
+    showModalBottomSheet(
+      context: context ,
+      builder: (context){
+        return BottomSheet(
+          onClosing: (){},
+          builder: (context){
+            return Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: FlatButton(
+                    child: Text("Ligar",
+                      style: TextStyle(color: Colors.red, fontSize: 20),
+                    ),
+                    onPressed: (){
+
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: FlatButton(
+                    child: Text("Editar",
+                      style: TextStyle(color: Colors.red, fontSize: 20),
+                    ),
+                    onPressed: (){
+
+                    },
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: FlatButton(
+                    child: Text("Excluir",
+                      style: TextStyle(color: Colors.red, fontSize: 20),
+                    ),
+                    onPressed: (){
+
+                    },
+                    ),
+                  ),
+                  
+                ],
+              ),
+            );
+          },
+        );
+      }
+    );
+  }
+
+  void _getAllContacts(){
+    helper.getAllContacts().then((list){
+      setState(() {
+        contacts = list; 
+      });
+    });
   }
 }
